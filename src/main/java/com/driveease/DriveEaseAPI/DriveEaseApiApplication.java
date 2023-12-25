2ffebcde9436e3725f4c0151470046ffbfce9ee8
Avishka Prabhath <avishkaprabha360@gmail.com>
2023-12-25 17:18:27 +0530

    initial commit

---
+++ src/main/java/com/driveease/DriveEaseAPI/DriveEaseApiApplication.java
@@ -0,0 +1,13 @@
+package com.driveease.DriveEaseAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriveEaseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriveEaseApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/driveease/DriveEaseAPI/DriveEaseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.driveease.DriveEaseAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriveEaseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
